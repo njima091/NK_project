293a03a19a4a4b3577dc264403c4d0badb570d8b
njima091 <eliont.njima@gmail.com>
2024-06-18 17:36:09 +0900

    Change a file

--- 2D_percolation_IC/main.py
+++ 2D_percolation_IC/main.py
@@ -1,1 +1,7 @@
 import random
+
+def Bernoulli(p):
+    ### Return Bernoulli random variable with parameter p ###
+    return p
+
+print(Bernoulli(0.5)) #This should be a random number,1 or 0, with equal probabilities
